42c63f129be5cea857a86186a0265024b4b7f2d7
Shuchita <shuchita@live.com>
2021-08-02 20:49:31 -0700

    rotate left

---
+++ rotLeft.js
@@ -0,0 +1,23 @@
+/*
+A left rotation operation on an array shifts each of the array's elements 1 unit 
+to the left. For example, if 2 left rotations are performed on array [1, 2, 3, 4, 5], 
+then the array would become [3, 4, 5, 1, 2]. Note that the lowest index item 
+moves to the highest index in a rotation. This is called a circular array.
+
+Given an array a of n integers and a number, d, perform d left rotations 
+on the array. Return the updated array to be printed as a single line of 
+space-separated integers.
+*/
+
+const a1 = [1, 2, 3, 4, 5];
+
+function rotLeft(a, d) {
+	let array = a;
+	for (let i = 0; i < d; i++) {
+		const first = array.shift();
+		array.push(first);
+	}
+	return array;
+}
+
+console.log(rotLeft(a1, 2));
